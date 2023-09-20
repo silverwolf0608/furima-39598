5a81f64b14df0cc8e5f8992a872874ed894c6574
silverwolf0608 <rstoyo.takuya.okamoto@gmail.com>
2023-09-20 17:03:22 +0900

    作成したusers.rbファイルにパラメータを記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    nick_name {'ニックネーム'}
+    first_name {'名'}
+    last_name {'性'}
+    kana_first_name {'メイ'}
+    kana_last_name {'セイ'}
+    birthday {'1999-01-31'}
+    email {'test@example'}
+    password {'pass012'}
+  end
+end
