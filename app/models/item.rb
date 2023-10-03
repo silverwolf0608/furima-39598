8326a26d23caf8068896ca000df3e23e96d18e72
silverwolf0608 <rstoyo.takuya.okamoto@gmail.com>
2023-10-03 11:35:05 +0900

    バリデーションを設定する

--- app/models/item.rb
+++ app/models/item.rb
@@ -26,4 +26,7 @@ class Item < ApplicationRecord
   validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :price, numericality: { only_integer: true, message: 'is out of setting range'}
 end
