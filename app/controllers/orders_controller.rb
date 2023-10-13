236ff5bc94b5e3b3d26139cfe1d72aea1c2f7fc4
silverwolf0608 <rstoyo.takuya.okamoto@gmail.com>
2023-10-13 15:20:16 +0900

    フォーム情報の保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,22 @@ class OrdersController < ApplicationController
   def index
     @purchase_record_address = PurchaseRecordAddress.new
   end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_record_address = PurchaseRecordAddress.new(purchase_record_params)
+    if @purchase_record_address.valid?
+      pay_item
+      @purchase_record_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def purchase_record_params
+    params.require(:purchase_record_address).permit(:post_code, :prefecture_id, :municipality, :street_address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
