98d5c6d7e75ec926fd5054c31ed375f59128e9cf
silverwolf0608 <rstoyo.takuya.okamoto@gmail.com>
2023-10-18 14:43:49 +0900

    手順3-4

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @purchase_record_address = PurchaseRecordAddress.new
     @item = Item.find(params[:item_id])
   end
@@ -12,6 +13,7 @@ class OrdersController < ApplicationController
       @purchase_record_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -19,11 +21,11 @@ class OrdersController < ApplicationController
   private
 
   def purchase_record_params
-    params.require(:purchase_record_address).permit(:post_code, :prefecture_id, :municipality, :street_address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token:params[:token])
+    params.require(:purchase_record_address).permit(:post_code, :prefecture_id, :municipality, :street_address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_69570e78029ea296a6408e37"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
     Payjp::Charge.create(
       amount: @item.price,
       card: purchase_record_params[:token],
